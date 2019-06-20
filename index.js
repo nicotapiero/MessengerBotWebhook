251846a42b3ff9126cbbb27a48710877770df5de
nicotapiero <tapiero@seas.upenn.edu>
2019-06-20 02:21:22 -0400

    new current pokemon method

--- index.js
+++ index.js
@@ -31,235 +31,12 @@ body_parser = require('body-parser'),
 
 
   const nodeyourmeme = require('nodeyourmeme');
-  
-
-  async function printAbout() {
-//var promise = ;
-var yeehaw = nodeyourmeme.search('knuckles'.substring(7));
-  	console.log(yeehaw.about);
-  }
-var yeehaw = nodeyourmeme.search('knuckles'.substring(7));
-/*  while (!(yeehaw.about)) {
-    		console.log('waiting');
-    	}*/
-    	console.log('hey');
-    	console.log(yeehaw.about);
-
-  //let result = await promise.about
-  //wait(5000);
-  printAbout();
-  //console.log('nico:::' + promise.about)
-
-// Sets server port and logs message on success
-app.listen(process.env.PORT || 1337, () => console.log('webhook is listening'));
-
-// Accepts POST requests at /webhook endpoint
-app.post('/webhook', (req, res) => {  
-
-  // Parse the request body from the POST
-  let body = req.body;
-
-  // Check the webhook event is from a Page subscription
-  if (body.object === 'page') {
-
-  	body.entry.forEach(function(entry) {
-
-      // Gets the body of the webhook event
-      let webhook_event = entry.messaging[0];
-      console.log(webhook_event);
-
-
-      // Get the sender PSID
-      let sender_psid = webhook_event.sender.id;
-      console.log('Sender ID: ' + sender_psid);
-
-      // Check if the event is a message or postback and
-      // pass the event to the appropriate handler function
-      if (webhook_event.message) {
-      	handleMessage(sender_psid, webhook_event.message);        
-      } else if (webhook_event.postback) {
-
-      	handlePostback(sender_psid, webhook_event.postback);
-      }
-      
-  });
-    // Return a '200 OK' response to all events
-    res.status(200).send('EVENT_RECEIVED');
-
-} else {
-    // Return a '404 Not Found' if event is not from a page subscription
-    res.sendStatus(404);
-}
-
-});
-
-// Accepts GET requests at the /webhook endpoint
-app.get('/webhook', (req, res) => {
-
-	/** UPDATE YOUR VERIFY TOKEN **/
-	const VERIFY_TOKEN = "nicostoken";
-
-  // Parse params from the webhook verification request
-  let mode = req.query['hub.mode'];
-  let token = req.query['hub.verify_token'];
-  let challenge = req.query['hub.challenge'];
-
-  // Check if a token and mode were sent
-  if (mode && token) {
-
-    // Check the mode and token sent are correct
-    if (mode === 'subscribe' && token === VERIFY_TOKEN) {
-
-      // Respond with 200 OK and challenge token from the request
-      console.log('WEBHOOK_VERIFIED');
-      res.status(200).send(challenge);
-
-  } else {
-      // Responds with '403 Forbidden' if verify tokens do not match
-      res.sendStatus(403);      
-  }
-}
-});
-
-async function handleMessage(sender_psid, received_message) {
-	let response;
-
-  // Checks if the message contains text
-  if (received_message.text) {    
-    // Create the payload for a basic text message, which
-    // will be added to the body of our request to the Send API
-   /* if (received_message.text.startsWith('Search:')) {
-    	//console.log(search)
-    	var text = nodeyourmeme.search(received_message.text.substring(7))
-    	while (!text) {
-
-    	}
-    	var text2 = await text.about;
-    	
-    	response = {
-    		"text": text2
-    	}
-    } else {*/
-
-
-    	if (received_message.text === 'Start') {
-    		var num = Math.floor(Math.random() * 803);
-
-    		response = {
-    		"attachment": {
-    		"type": "template",
-    		"payload": {
-    			"template_type": "generic",
-    			"elements": [{
-    				"title": "‌‌A wild pokémon has appeared!",
-    				"subtitle": 'Guess the pokémon and type "catch <pokémon>" to catch it!',
-    				"image_url": "https://img.pokemondb.net/artwork/large/" + pokemon[num] + ".jpg",
-    			}]
-    		}
-    	}
-    }
-    	} else {
-    	response = {
-    		"text": `You sent the message: "${received_message.text}". Now send me an attachment!`
-    	}
-    }
-    //}
-} else if (received_message.attachments) {
-    // Get the URL of the message attachment
-    let attachment_url = received_message.attachments[0].payload.url;
-    response = {
-    	"attachment": {
-    		"type": "template",
-    		"payload": {
-    			"template_type": "generic",
-    			"elements": [{
-    				"title": "Is this the right picture?",
-    				"subtitle": "Tap a button to answer.",
-    				"image_url": attachment_url,
-    				"buttons": [
-    				{
-    					"type": "postback",
-    					"title": "Yes!",
-    					"payload": "yes",
-    				},
-    				{
-    					"type": "postback",
-    					"title": "No!",
-    					"payload": "no",
-    				}
-    				],
-    			}]
-    		}
-    	}
-    }
-} 
-
-  // Send the response message
-  callSendAPI(sender_psid, response);    
-}
-
-function handlePostback(sender_psid, received_postback) {
-	console.log('ok')
-	let response;
-  // Get the payload for the postback
-  let payload = received_postback.payload;
-
-  // Set the response based on the postback payload
-  if (payload === 'yes') {
-  	response = { "text": "Thanks!" }
-  } else if (payload === 'no') {
-  	response = { "text": "Oops, try sending another image." }
-  }
-  // Send the message to acknowledge the postback
-  callSendAPI(sender_psid, response);
-}
-
-function callSendAPI(sender_psid, response) {
-  // Construct the message body
-  let request_body = {
-  	"recipient": {
-  		"id": sender_psid
-  	},
-  	"message": response
-  }
-
-  // Send the HTTP request to the Messenger Platform
-  request({
-  	"uri": "https://graph.facebook.com/v2.6/me/messages",
-  	"qs": { "access_token": PAGE_ACCESS_TOKEN },
-  	"method": "POST",
-  	"json": request_body
-  }, (err, res, body) => {
-  	if (!err) {
-  		console.log('message sent!')
-  	} else {
-  		console.error("Unable to send message:" + err);
-  	}
-  }); 
-}
 
 
 
-
-
-
-
-
-
-
-function threeNum(num) {
-if (num < 10) {
-	return "00"+num;
-} else if (num < 100){
-	return "0"+num;
-} else {
-	return ""+num;
-}
-}
-
-
-
-
+var currentPokemon;
+function resetCurrentPokemon() {
+var number = Math.floor(Math.random() * 803);
 var pokemon = ["Bulbasaur",
 "Ivysaur",
 "Venusaur",
@@ -1062,6 +839,260 @@ var pokemon = ["Bulbasaur",
 "Necrozma",
 "Magearna",
 "Marshadow"];
+
+var string = pokemon[number];
+currentPokemon = string.toLowerCase();
+
+}
+resetCurrentPokemon();
+
+
+
+  
+
+  async function printAbout() {
+//var promise = ;
+var yeehaw = nodeyourmeme.search('knuckles'.substring(7));
+  	console.log(yeehaw.about);
+  }
+var yeehaw = nodeyourmeme.search('knuckles'.substring(7));
+
+function printPokemon() {
+var num = Math.floor(Math.random() * 803);
+    		//console.log("https://img.pokemondb.net/artwork/large/" + pokemon[num] + ".jpg")
+}
+printPokemon();
+console.log("currentPokemon is" + currentPokemon)
+/*  while (!(yeehaw.about)) {
+    		console.log('waiting');
+    	}*/
+    	console.log('hey');
+    	console.log(yeehaw.about);
+
+
+  //let result = await promise.about
+  //wait(5000);
+  printAbout();
+  //console.log('nico:::' + promise.about)
+
+// Sets server port and logs message on success
+app.listen(process.env.PORT || 1337, () => console.log('webhook is listening'));
+
+// Accepts POST requests at /webhook endpoint
+app.post('/webhook', (req, res) => {  
+
+  // Parse the request body from the POST
+  let body = req.body;
+
+  // Check the webhook event is from a Page subscription
+  if (body.object === 'page') {
+
+  	body.entry.forEach(function(entry) {
+
+      // Gets the body of the webhook event
+      let webhook_event = entry.messaging[0];
+      console.log(webhook_event);
+
+
+      // Get the sender PSID
+      let sender_psid = webhook_event.sender.id;
+      console.log('Sender ID: ' + sender_psid);
+
+      // Check if the event is a message or postback and
+      // pass the event to the appropriate handler function
+      if (webhook_event.message) {
+      	handleMessage(sender_psid, webhook_event.message);        
+      } else if (webhook_event.postback) {
+
+      	handlePostback(sender_psid, webhook_event.postback);
+      }
+      
+  });
+    // Return a '200 OK' response to all events
+    res.status(200).send('EVENT_RECEIVED');
+
+} else {
+    // Return a '404 Not Found' if event is not from a page subscription
+    res.sendStatus(404);
+}
+
+});
+
+// Accepts GET requests at the /webhook endpoint
+app.get('/webhook', (req, res) => {
+
+	/** UPDATE YOUR VERIFY TOKEN **/
+	const VERIFY_TOKEN = "nicostoken";
+
+  // Parse params from the webhook verification request
+  let mode = req.query['hub.mode'];
+  let token = req.query['hub.verify_token'];
+  let challenge = req.query['hub.challenge'];
+
+  // Check if a token and mode were sent
+  if (mode && token) {
+
+    // Check the mode and token sent are correct
+    if (mode === 'subscribe' && token === VERIFY_TOKEN) {
+
+      // Respond with 200 OK and challenge token from the request
+      console.log('WEBHOOK_VERIFIED');
+      res.status(200).send(challenge);
+
+  } else {
+      // Responds with '403 Forbidden' if verify tokens do not match
+      res.sendStatus(403);      
+  }
+}
+});
+
+async function handleMessage(sender_psid, received_message) {
+	let response;
+
+  // Checks if the message contains text
+  if (received_message.text) {    
+    // Create the payload for a basic text message, which
+    // will be added to the body of our request to the Send API
+   /* if (received_message.text.startsWith('Search:')) {
+    	//console.log(search)
+    	var text = nodeyourmeme.search(received_message.text.substring(7))
+    	while (!text) {
+
+    	}
+    	var text2 = await text.about;
+    	
+    	response = {
+    		"text": text2
+    	}
+    } else {*/
+
+
+    	if (received_message.text === 'Start') {
+    		
+    		console.log("https://img.pokemondb.net/artwork/large/" + currentPokemon + ".jpg")
+
+    		response = {
+    		"attachment": {
+    		"type": "template",
+    		"payload": {
+    			"template_type": "generic",
+    			"elements": [{
+    				"title": "‌‌A wild pokémon has appeared!",
+    				"subtitle": 'Guess the pokémon and type "catch <pokémon>" to catch it!',
+    				"image_url": "https://img.pokemondb.net/artwork/large/" + currentPokemon + ".jpg",
+    			}]
+    		}
+    	}
+    }
+    	} else {
+    	response = {
+    		"text": `You sent the message: "${received_message.text}". Now send me an attachment!`
+    	}
+    }
+    //}
+} else if (received_message.attachments) {
+    // Get the URL of the message attachment
+    let attachment_url = received_message.attachments[0].payload.url;
+    response = {
+    	"attachment": {
+    		"type": "template",
+    		"payload": {
+    			"template_type": "generic",
+    			"elements": [{
+    				"title": "Is this the right picture?",
+    				"subtitle": "Tap a button to answer.",
+    				"image_url": attachment_url,
+    				"buttons": [
+    				{
+    					"type": "postback",
+    					"title": "Yes!",
+    					"payload": "yes",
+    				},
+    				{
+    					"type": "postback",
+    					"title": "No!",
+    					"payload": "no",
+    				}
+    				],
+    			}]
+    		}
+    	}
+    }
+} 
+
+  // Send the response message
+  callSendAPI(sender_psid, response);    
+}
+
+function handlePostback(sender_psid, received_postback) {
+	console.log('ok')
+	let response;
+  // Get the payload for the postback
+  let payload = received_postback.payload;
+
+  // Set the response based on the postback payload
+  if (payload === 'yes') {
+  	response = { "text": "Thanks!" }
+  } else if (payload === 'no') {
+  	response = { "text": "Oops, try sending another image." }
+  }
+  // Send the message to acknowledge the postback
+  callSendAPI(sender_psid, response);
+}
+
+function callSendAPI(sender_psid, response) {
+  // Construct the message body
+  let request_body = {
+  	"recipient": {
+  		"id": sender_psid
+  	},
+  	"message": response
+  }
+
+  // Send the HTTP request to the Messenger Platform
+  request({
+  	"uri": "https://graph.facebook.com/v2.6/me/messages",
+  	"qs": { "access_token": PAGE_ACCESS_TOKEN },
+  	"method": "POST",
+  	"json": request_body
+  }, (err, res, body) => {
+  	if (!err) {
+  		console.log('message sent!')
+  	} else {
+  		console.error("Unable to send message:" + err);
+  	}
+  }); 
+}
+
+
+
+
+
+
+
+
+
+
+function threeNum(num) {
+if (num < 10) {
+	return "00"+num;
+} else if (num < 100){
+	return "0"+num;
+} else {
+	return ""+num;
+}
+}
+
+
+
+
+
+
+
+
+
+
+
 
 
 
